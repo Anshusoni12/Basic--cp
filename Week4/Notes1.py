0073eee1426578e7d43bde9108d0a72383bc5253
Anshu Soni <varshasoni9291@gmail.com>
2025-08-20 14:55:21 +0530

    Create Notes1.py

---
+++ Week4/Notes1.py
@@ -0,0 +1,25 @@
+# print(list(range(0,6)))
+# for i in range(1,10,2):
+#     print(i)
+# for i in range(1,10,2):
+#     print(i*i,end=" ")
+
+# n= int(input("Enter a number:"))
+# for i in range(1,n+1):
+#     print(i,end=" ")
+
+# n=int(input("Enter a num:"))
+# for i in range(1,n+1,2):
+#     print(i,end=" ")
+
+# A=2
+# B=3
+# p=1
+# for i in range(1,B+1):
+#     p=p*A
+# print(p,end=" ")
+
+for i in range(1,21):
+    if i%3==0:
+        continue
+    print(i,end=' ')
