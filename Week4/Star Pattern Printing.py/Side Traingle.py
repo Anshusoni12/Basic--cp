91ecd2e81e3f98df871ecc9f6230a1f17946f22b
Anshu Soni <varshasoni9291@gmail.com>
2025-09-07 17:24:25 +0530

    Star Pattern
    Star Pattern

---
+++ Week4/Star Pattern Printing.py/Side Traingle.py
@@ -0,0 +1,14 @@
+#For Upper Half Triangle
+n=int(input("Enter number of rows:" ))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+
+#For Lower Half Triangle
+
+for i in range(n-1,0,-1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+
---
+++ Week4/Star Pattern Printing.py/Small C.py
@@ -0,0 +1,9 @@
+n=5
+rows=n
+cols=n
+for i in range(rows):
+    if i==2:
+        print(" * ")
+    else:
+        print(" *  * ")
+print()
