29d3c1ec18231e1e76e1f44630c07556717d2ccb
Anshu Soni <varshasoni9291@gmail.com>
2025-09-12 10:13:59 +0530

    Update 7.Separateoddeven.py

--- List Sheet/7.Separateoddeven.py
+++ List Sheet/7.Separateoddeven.py
@@ -1,10 +1,13 @@
 arr=list(map(int,input().split()))
 for i in range(len(arr)):
     if arr[i]%2==0:
-        print(arr[i])
+        print(arr[i],end=" ")
+print()
 
 for i in range(len(arr)):
     if arr[i]%2!=0:
-        print(arr[i])
+        print(arr[i],end=" ")
 print()
+    
+
 
